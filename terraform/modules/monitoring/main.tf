0041dfb71a3065d2481bed1a9d1ff8f933ad4bbd
Fausto Soares Machado <faustosoares.m@gmail.com>
2025-12-03 15:09:09 -0300

    [feature] teste threshold

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -2,16 +2,17 @@
 resource "aws_cloudwatch_metric_alarm" "cluster_cpu_utilization" {
   alarm_name                = "teddy-ecs-high-cpu-alarm"
   comparison_operator       = "GreaterThanThreshold"
-  evaluation_periods        = 1
+  evaluation_periods        = 5
   metric_name               = "CPUUtilization"
   namespace                 = "AWS/ECS"
-  period                    = 60 # segundos nt
+  period                    = 60 # segundos
   statistic                 = "Average"
-  threshold                 = 1.0 # Acima de 80% de CPU
+  threshold                 = 1.5 # Acima de 80% de CPU
   alarm_description         = "Alerta se a utilização média da CPU do cluster for alta."
   unit                      = "Percent"
   
   dimensions = {
     ClusterName = var.cluster_name
+    ServiceName = "teddy-service"
   }
 }
