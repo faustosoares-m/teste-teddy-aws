747362fc8c4d766346a8a407677cdea9b01fc226
Fausto Soares Machado <faustosoares.m@gmail.com>
2025-12-03 09:41:23 -0300

    [feature] adicionando backend para o terraform state

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,18 +17,18 @@ module "ecr" {
 }
 
 module "monitoring" {
-  source    = "./modules/monitoring"
+  source       = "./modules/monitoring"
   cluster_name = module.fargate.cluster_name # Passando o nome do cluster do output do fargate
 }
 
 module "fargate" {
-  source             = "./modules/fargate"
-  
+  source = "./modules/fargate"
+
   # Passando IDs de rede (corrigidos)
-  vpc_id             = module.network.vpc_id 
-  private_subnet_ids = module.network.private_subnet_ids 
-  public_subnet_ids  = module.network.public_subnet_ids 
-  
+  vpc_id             = module.network.vpc_id
+  private_subnet_ids = module.network.private_subnet_ids
+  public_subnet_ids  = module.network.public_subnet_ids
+
   security_group_id  = module.security.security_group_id
   ecr_image_url      = "${module.ecr.repository_url}:latest"
   execution_role_arn = module.iam.ecs_execution_role_arn
